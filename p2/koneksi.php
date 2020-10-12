e2f949d5db308fdbe9b722ff4a2fb783eb10c9a8
j3c118120 <63805491+j3c118120@users.noreply.github.com>
2020-10-12 20:33:42 +0700

    Add files via upload
    p2

---
+++ p2/koneksi.php
@@ -0,0 +1,8 @@
+<?php
+   $host   = "sql100.epizy.com";//"localhost";
+   $user   = "epiz_26771902";//"root";
+   $psw    = "cewt5pBekpVMI";//"";
+   $db_name = "epiz_26771902_j3c118120";//"pabw";
+   $koneksi = mysqli_connect($host, $user, $psw, $db_name);
+
+?>
